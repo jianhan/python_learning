39a725eb17769e1d7ae458986e7953b35196c818
Jian Han <jianhan@mail.com>
2018-04-15 02:01:35 +1000

    [DEV] Added list

---
+++ cookbook_3rd/ch01/unpack.py
@@ -0,0 +1,19 @@
+"""
+Problem
+You have an N-element tuple or sequence that you would like to unpack into a collection
+of N variable
+"""
+
+# simple example
+p = (4, 5)
+x, y = p
+print(x)
+print(y)
+# array
+
+# list example
+data = ["Jim Smith", 24, (1, 2, 3)]
+name, age, price = data
+print(name)
+print(age)
+print(price)
