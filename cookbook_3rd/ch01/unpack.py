214d0f16497016c8a5deca4f0a89d522f0628d61
Jian Han <jianhan@mail.com>
2018-04-15 02:35:01 +1000

    [DEV] Added sequence of tuples of varying length.

--- cookbook_3rd/ch01/unpack.py
+++ cookbook_3rd/ch01/unpack.py
@@ -41,3 +41,26 @@ print(drop_first_last(grades))
 record = ('Dave', 'dave@example.com', '773-555-1212', '847-555-1212')
 name, email, *phone_numbers = record
 print(name, email, phone_numbers)
+
+# It is worth noting that the star syntax can be especially useful when iterating over a
+# sequence of tuples of varying length. For example, perhaps a sequence of tagged tuples:
+records = [
+    ('foo', 1, 2),
+    ('bar', 'hello'),
+    ('foo', 3, 4),
+]
+
+
+def do_foo(x, y):
+    print('foo', x, y)
+
+
+def do_bar(s):
+    print('bar', s)
+
+
+for tag, *args in records:
+    if tag == 'foo':
+        do_foo(*args)
+    elif tag == 'bar':
+        do_bar(*args)
