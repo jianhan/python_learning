ea0bb83fdfd63f165456a4adafd0ff1238138d43
Jian Han <jianhan@mail.com>
2018-04-15 02:14:59 +1000

    [DEV] Added start expression

--- cookbook_3rd/ch01/unpack.py
+++ cookbook_3rd/ch01/unpack.py
@@ -2,6 +2,10 @@
 Problem
 You have an N-element tuple or sequence that you would like to unpack into a collection
 of N variable
+
+Tips:
+Unpacking actually works with any object that happens to be iterable, not just tuples or
+lists. This includes strings, files, iterators, and generators. For examp
 """
 
 # simple example
@@ -17,3 +21,18 @@ name, age, price = data
 print(name)
 print(age)
 print(price)
+
+"""
+Problem
+You need to unpack N elements from an iterable, but the iterable may be longer than N
+elements, causing a “too many values to unpack” exception.
+"""
+
+
+def drop_first_last(grades):
+    first, *middle, last = grades
+    return middle
+
+
+grades = [1, 2, 3, 4, 5, 6]
+print(drop_first_last(grades))
