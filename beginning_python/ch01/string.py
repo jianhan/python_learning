eb97eeaf27197c755d7bcd0f0e7dce482f35b005
Jian Han <jianhan@mail.com>
2018-04-16 20:17:30 +1000

    Added str and repr func

--- beginning_python/ch01/string.py
+++ beginning_python/ch01/string.py
@@ -1,2 +1,23 @@
 # concat string
 print("I am " 'json')
+
+# concat with variable
+
+first = "James"
+last = "bond"
+
+# The following will not work
+# print(first last)
+print(first + last)
+
+"""
+With str, you convert a value into a string in some reasonable
+fashion that will probably be understood by a user, 
+"""
+print(str("Hello,\nworld!"))
+
+"""
+If you use repr, however, you will generally get a
+representation of the value as a legal Python expression.
+"""
+print(repr("Hello,\nworld!"))
