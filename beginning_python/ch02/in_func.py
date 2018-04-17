b5f48cf06e7729f7eb04e135a354f8ffaf834bbb
Jian Han <jianhan@mail.com>
2018-04-17 22:37:33 +1000

    Added more example for assignment

--- beginning_python/ch02/in_func.py
+++ beginning_python/ch02/in_func.py
@@ -8,3 +8,23 @@ if ["jones", '9843'] in database: print('Access granted')
 
 l = list("hello world")
 print(l, ''.join(l))
+
+# assign slices
+name = list('Perl')
+name[2:] = list('ar')
+print(name, ''.join(name))
+
+# replace with different length
+name = list('Perl')
+name[1:] = list('ython')
+print(name)
+
+# insert without replacing any of the original
+numbers = [1, 5]
+numbers[1:1] = [2, 3, 4]
+numbers[2:1] = ["test", "test1"]
+print(numbers)
+
+# reverse for deletion
+numbers[1:6] = []
+print(numbers)
