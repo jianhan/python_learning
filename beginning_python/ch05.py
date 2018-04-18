f3f47a79819049c3e6468c5d238b8a2726a79737
Jian Han <jianhan@mail.com>
2018-04-18 21:41:09 +1000

    Example for multi assign from a function return

--- beginning_python/ch05.py
+++ beginning_python/ch05.py
@@ -6,3 +6,10 @@ print(x, y, z)
 
 x, y = y, x
 print(x, y)
+
+# You can then use the popitem method, which does just that, returning the pair as a tuple. Then you can unpack the
+# returned tuple directly into two variables
+
+scoundrel = {'name': 'Robin', 'girlfriend': 'Marion'}
+key, value = scoundrel.popitem()
+print(key, value)
