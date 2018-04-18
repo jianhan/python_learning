243fd34cf9358cfd5072e42bc1fc636439048d03
Jian Han <jianhan@mail.com>
2018-04-18 21:20:08 +1000

    Added template parse

--- beginning_python/ch04.py
+++ beginning_python/ch04.py
@@ -8,3 +8,18 @@ print(d)
 # It can also be used with keyword arguments, as follows:
 d = dict(name='Gumby', age=42)
 print(d)
+
+# string formatting
+phonebook = {'Beth': '9102', 'Alice': '2341', 'Cecil': '3258'}
+
+print("Cecil's phone number is {Cecil}.".format_map(phonebook))
+
+# template parse
+template = '''<html>
+<head><title>{title}</title></head>
+<body>
+<h1>{title}</h1>
+<p>{text}</p>
+</body>'''
+data = {'title': 'My Home Page', 'text': 'Welcome to my home page!'}
+print(template.format_map(data))
