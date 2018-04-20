05a707f95e8fde8e6924b6a65d1e4d08978f7752
Jian Han <jianhan@mail.com>
2018-04-20 23:38:43 +1000

    A better solution

--- beginning_python/ch05.py
+++ beginning_python/ch05.py
@@ -43,3 +43,12 @@ for x in range(3):
     for y in range(3):
         result.append((x, y))
 print(result)
+
+# a better solution
+
+girls = ['alice', 'bernice', 'clarice']
+boys = ['chris', 'arnold', 'bob']
+letterGirls = {}
+for girl in girls:
+    letterGirls.setdefault(girl[0], []).append(girl)
+print([b + '+' + g for b in boys for g in letterGirls[b[0]]])
