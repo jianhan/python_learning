050ca9ca59b29cb8f18520dfd5a9188daf73a430
Jian Han <jianhan@mail.com>
2018-04-21 09:24:36 +1000

    more example of param

--- beginning_python/ch05.py
+++ beginning_python/ch05.py
@@ -99,3 +99,8 @@ def hello_4(name, greeting='Hello', punctuation='!'):
 
 
 hello_4('Mars')
+hello_4('Mars', 'Howdy')
+hello_4('Mars', 'Howdy', '...')
+hello_4('Mars', punctuation='.')
+hello_4('Mars', greeting='Top of the morning to ya')
+hello_4()
