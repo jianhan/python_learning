23be7e75556bb6534516195d98c5b1ac0acebb31
Jian Han <jianhan@mail.com>
2018-04-23 20:36:58 +1000

    simple sort example

--- fluent_python/ch01/card.py
+++ fluent_python/ch01/card.py
@@ -28,5 +28,29 @@ print(beer_card)
 print(choice(deck))
 
 for card in deck:
+    print(card, card.rank, "****")
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+
+# print(Card('Q', 'hearts') in deck, Card('7', 'beasts') in deck)
+# print(deck.ranks)
+for card in sorted(deck, key=spades_high):
     print(card)
-print(Card('Q', 'hearts') in deck, Card('7', 'beasts') in deck)
+
+# sort example
+
+ns = [4, 5, 1, 2, 10, 22]
+
+
+def number_sort(n):
+    return n
+
+
+for n in sorted(ns, key=number_sort):
+    print(n)
