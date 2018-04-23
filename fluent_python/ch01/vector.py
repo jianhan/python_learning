460994a7a944f22d466652258c4c9b3a67cacc6f
Jian Han <jianhan@mail.com>
2018-04-23 21:15:00 +1000

    vector example

---
+++ fluent_python/ch01/vector.py
@@ -0,0 +1,25 @@
+from math import hypot
+
+
+class Vector:
+    def __init__(self, x=0, y=0):
+        self.x, self.y = x, y
+
+    def __repr__(self):
+        # The __repr__ special method is called by the repr built-in to get string representation
+        # of the object for inspection
+        return 'Vector(%r, %r)' % (self.x, self.y)
+
+    def __abs__(self):
+        return hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar)
