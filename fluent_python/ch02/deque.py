584dffc70580ac6e99c16dfc3b56be583d3c9cff
Jian Han <jianhan@mail.com>
2018-04-25 19:51:45 +1000

    matrix

---
+++ fluent_python/ch02/deque.py
@@ -0,0 +1,6 @@
+from collections import deque
+
+dq = deque(range(10), maxlen=10)
+print(dq)
+dq.rotate(3)
+print(dq)
---
+++ practical_ML_with_python/ch01/matrix.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+m = np.array([[1, 5, 2],
+              [4, 7, 4],
+              [2, 0, 9]])
+print(m, m.shape)
