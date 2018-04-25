3a31f71f72959f1022a6122ded70ce36c6d00359
Jian Han <jianhan@mail.com>
2018-04-25 12:10:30 +1000

    invalid example

--- fluent_python/ch02/tuple.py
+++ fluent_python/ch02/tuple.py
@@ -63,3 +63,8 @@ print(l)
 board = [['_'] * 3 for i in range(3)]
 board[1][2] = "X"
 print(board)
+
+# build list incorrect example
+weird_board = [['_'] * 3] * 3
+weird_board[1][2] = 'O'
+print(weird_board)
