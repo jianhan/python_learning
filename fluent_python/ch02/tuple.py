bfc05fc8881bcef5f8deb611280d4037387e6cf8
Jian Han <jianhan@mail.com>
2018-04-25 10:32:25 +1000

    unpacking and function call

--- fluent_python/ch02/tuple.py
+++ fluent_python/ch02/tuple.py
@@ -5,3 +5,19 @@ for passport in sorted(traveler_ids):
     print('%s/%s' % passport)
 for country, _ in traveler_ids:
     print(country)
+
+# unpack
+lax_coordinates = (33.9425, -118.408056)
+latitude, longitude = lax_coordinates
+print(latitude, longitude)
+
+
+# unpack with function call
+
+def hello(first_name, last_name):
+    print(first_name + " " + last_name)
+
+
+hello(first_name="James", last_name="Smith")
+fullname = ("James", "Smith")
+hello(*fullname)
