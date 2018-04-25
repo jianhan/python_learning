bdcb7eab1b744619128800768b21584b3ca481c9
Jian Han <jianhan@mail.com>
2018-04-25 10:40:54 +1000

    named tuple

--- fluent_python/ch02/tuple.py
+++ fluent_python/ch02/tuple.py
@@ -37,3 +37,10 @@ fmt = '{:15} | {:9.4f} | {:9.4f}'
 for name, cc, pop, (latitude, longitude) in metro_areas:
     if longitude <= 0:  #
         print(fmt.format(name, latitude, longitude))
+
+# named tuple
+from collections import namedtuple
+
+City = namedtuple('City', 'name country population coordinates')
+tokyo = City('Tokyo', 'JP', 36.933, (35.689722, 139.691667))
+print(tokyo, tokyo.population, tokyo.name)
