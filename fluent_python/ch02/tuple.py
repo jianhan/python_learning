06d191fa474e97db9cf3433d80fd5d09c8010f93
Jian Han <jianhan@mail.com>
2018-04-25 11:01:28 +1000

    make example of tuple

--- fluent_python/ch02/tuple.py
+++ fluent_python/ch02/tuple.py
@@ -43,4 +43,8 @@ from collections import namedtuple
 
 City = namedtuple('City', 'name country population coordinates')
 tokyo = City('Tokyo', 'JP', 36.933, (35.689722, 139.691667))
-print(tokyo, tokyo.population, tokyo.name)
+print(tokyo, tokyo.population, tokyo.name, tokyo._fields)
+LatLong = namedtuple('LatLong', 'lat long')
+delhi_data = ('Delhi NCR', 'IN', 21.935, LatLong(28.613889, 77.208889))
+delhi = City._make(delhi_data)
+print(delhi._asdict())
