d6766c14ee5bf3a153f4fc05eb6e60a3e79d8703
Jian Han <jianhan@mail.com>
2018-04-25 20:06:16 +1000

    Eigen Decompose

--- practical_ML_with_python/ch01/matrix.py
+++ practical_ML_with_python/ch01/matrix.py
@@ -16,3 +16,12 @@ print('Matrix inverse:\n', m_inv, '\n')
 iden_m = np.dot(m, m_inv)
 iden_m = np.round(np.abs(iden_m), 0)
 print('Product of matrix and its inverse:\n', iden_m)
+
+# eigendecomposition
+
+m = np.array([[1, 5, 2],
+              [4, 7, 4],
+              [2, 0, 9]])
+eigen_vals, eigen_vecs = np.linalg.eig(m)
+print('Eigen Values:', eigen_vals, '\n')
+print('Eigen Vectors:\n', eigen_vecs)
