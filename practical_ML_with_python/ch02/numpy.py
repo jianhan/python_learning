bb87ebeee034faef932551bdc93664eace52e733
Jian Han <jianhan@mail.com>
2018-04-29 10:51:46 +1000

    conditional selection

--- practical_ML_with_python/ch02/numpy.py
+++ practical_ML_with_python/ch02/numpy.py
@@ -14,5 +14,11 @@ print(arr)
 arr = np.arange(9).reshape(3, 3)
 # In this example we have provided an array in which the first part identifies the rows we want to access
 # and the second identifies the columns which we want to address. This is quite similar to providing a
+
 # collective element-wise address.
 print(arr[[0, 1, 2], [1, 0, 0]])
+
+# boolean index
+cities = np.array(["delhi", "bangalore", "mumbai", "chennai", "bhopal"])
+city_data = np.random.randn(5, 3)
+print(city_data[cities == "delhi"], "***", city_data[city_data > 0])
