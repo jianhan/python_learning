f16fa95f4151f483d9673417564ced233fa5c2a7
Jian Han <jianhan@mail.com>
2018-04-30 21:43:52 +1000

    dictcomps

--- fluent_python/ch02/hash.py
+++ fluent_python/ch02/hash.py
@@ -27,3 +27,21 @@ c = dict(zip(['one', 'two', 'three'], [1, 2, 3]))
 d = dict([('two', 2), ('one', 1), ('three', 3)])
 e = dict({'three': 3, 'one': 1, 'two': 2})
 print(a == b == c == d == e)
+
+# Examples of dict comprehensions
+
+DIAL_CODES = [
+    (86, 'China'),
+    (91, 'India'),
+    (1, 'United States'),
+    (62, 'Indonesia'),
+    (55, 'Brazil'),
+    (92, 'Pakistan'),
+    (880, 'Bangladesh'),
+    (234, 'Nigeria'),
+    (7, 'Russia'),
+    (81, 'Japan'),
+]
+country_code = {country: code for code, country in DIAL_CODES}
+print(country_code)
+print({code: country.upper() for country, code in country_code.items() if code < 66})
